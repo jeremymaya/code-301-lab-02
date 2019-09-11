74fc78b5520bd3a014c6d7d61da40aa675828e68
jeremiah <jeremiah.kim@hotmail.com>
2019-09-11 12:43:02 -0700

    option values for select created

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 console.log('working');
 
 const allHorns = [];
+const keywords = [];
 
 // const testHorn =   {
 //     "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
@@ -33,24 +34,48 @@ $.get('/data/page-1.json', data => {
     data.forEach(horn => {
         let tempHorn = new Horn(horn);
         tempHorn.render();
+        
         // console.log(tempHorn);
     });
+    getKeywords(allHorns);
+
     // console.log(allHorns);
 });
 
 
 // Render function
+// Add keyword as option
 
 Horn.prototype.render = function() {
-
+    
     const myTemplate = $('#photo-template').html();
     const $newSection = $('<section></section>');
     $newSection.html(myTemplate);
     
+    $newSection.attr('data-keyword', this.keyword);
     $newSection.find('h2').text(this.title);
     $newSection.find('img').attr('src', this.image_url);
     $newSection.find('p').text(this.description);
     
+
     // console.log($newSection);
     $('main').append($newSection);
 };
+
+// Generating Select element from allHorn
+
+const getKeywords = (arr) => {
+    arr.forEach(horn => {
+        if(!keywords.includes(horn.keyword)){
+            keywords.push(horn.keyword);
+        }
+    })
+    console.log(keywords);
+}               
+
+
+// Add event listener to the select list to filter images
+
+// Show/Hide images depending on the selected keyword
+
+
