612c6d377ef9de6b2d2c86a8d73b356cb74e1b3d
spinetree <steve@spinetree.org>
2019-09-11 09:58:30 -0700

    AJAX working

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@ console.log('working');
 
 const allHorns = [];
 
-const testHorn =   {
-    "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-    "title": "UniWhal",
-    "description": "A unicorn and a narwhal nuzzling their horns",
-    "keyword": "narwhal",
-    "horns": 1
-  };
+// const testHorn =   {
+//     "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+//     "title": "UniWhal",
+//     "description": "A unicorn and a narwhal nuzzling their horns",
+//     "keyword": "narwhal",
+//     "horns": 1
+//   };
 
 // Constructor function
+
 function Horn(horn) {
     
     this.image_url = horn.image_url; 
@@ -24,10 +25,22 @@ function Horn(horn) {
     allHorns.push(this);
 };
 
-new Horn(testHorn);
-console.log(allHorns);
+// new Horn(testHorn);
+
 
 // AJAX
+
+$.get('/data/page-1.json', data => {
+
+    data.forEach(horn => {
+
+        let tempHorn = new Horn(horn);
+        // render
+        console.log(tempHorn);
+    });
+    console.log(allHorns);
+
+})
 
 
 // Render function
